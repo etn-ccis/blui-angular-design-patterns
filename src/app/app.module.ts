5a37a167beba835e3df45785866e696974e3e777
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-03-26 09:00:47 -0400

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatButtonModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],

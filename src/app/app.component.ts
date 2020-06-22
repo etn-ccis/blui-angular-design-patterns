71a1595aaccaf5a6acaafb2f4e7c91eed6845c49
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-06-22 10:31:32 -0400

    Add drawer, routes, run prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { StateService } from './services/state.service';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ViewportService } from './services/viewport.service';
+import * as PXBColors from '@pxblue/colors';
+import { DrawerLayoutVariantType } from '@pxblue/angular-components';
+import { DrawerItem, ROUTEES } from './app-routing.module';
+const iconSet = require('@pxblue/icons-svg/icons.svg');
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-design-patterns';
+    title = 'angular-design-patterns';
+    designPatternDetails: string;
+    colors: Record<string, any>;
+    variant: DrawerLayoutVariantType = 'persistent';
+    routes = ROUTEES;
+    selected: string;
+
+    constructor(
+        private router: Router,
+        public readonly stateService: StateService,
+        private readonly matIconRegistry: MatIconRegistry,
+        private readonly domSanitizer: DomSanitizer,
+        private readonly viewportService: ViewportService
+    ) {
+        this.colors = PXBColors;
+        this.matIconRegistry.addSvgIconSetInNamespace(
+            'px-icons',
+            this.domSanitizer.bypassSecurityTrustResourceUrl(iconSet)
+        );
+    }
+
+    select(route: DrawerItem): void {
+        this.router.navigate([route.path]);
+        this.selected = route.title;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { UnCamelCasePipe } from './shared/un-camel-case.pipe';
+import { GroupByPipe } from './shared/groupby.pipe';
+import { ObjectToList } from './shared/key-value.pipe';
+import { DataListComponent } from './pages/data-list/data-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormValidationComponent } from './pages/form-validation/form-validation.component';
+import { ActionListComponent } from './pages/action-list/action-list.component';
+import { AppBarComponent } from './pages/app-bar/app-bar.component';
+import { DrawerLayoutModule, DrawerModule, EmptyStateModule } from '@pxblue/angular-components';
+import { PlaceholderComponent } from './pages/placeholder/placeholder.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UnCamelCasePipe,
+        GroupByPipe,
+        ObjectToList,
+        DataListComponent,
+        FormValidationComponent,
+        ActionListComponent,
+        AppBarComponent,
+        PlaceholderComponent,
+    ],
+    imports: [
+        DrawerLayoutModule,
+        DrawerModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        FlexLayoutModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatListModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatTabsModule,
+        EmptyStateModule,
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

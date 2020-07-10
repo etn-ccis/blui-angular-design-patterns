c189c47b38a45b92c9f03f944c2881aaf3568b65
daileytj <daileytj89@gmail.com>
2020-07-10 10:59:17 -0400

    Add basic-bottom-sheet example

---
+++ src/app/pages/overlays/basic-bottom-sheet/bottom-sheet/bottom-sheet.ts
@@ -0,0 +1,29 @@
+import { Component, ViewEncapsulation, NgModule } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { InfoListItemModule } from '@pxblue/angular-components';
+
+@Component({
+    selector: 'bottom-sheet',
+    templateUrl: './bottom-sheet.html',
+    styleUrls: ['./bottom-sheet.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class BottomSheet {
+    constructor(private readonly _bottomSheetRef: MatBottomSheetRef<BottomSheet>) {}
+
+    // this is the placeholder for actual functionality
+    openLink(event: MouseEvent): void {
+        this._bottomSheetRef.dismiss();
+        event.preventDefault();
+    }
+}
+
+@NgModule({
+    imports: [CommonModule, MatIconModule, MatListModule, InfoListItemModule],
+    declarations: [BottomSheet],
+    providers: [],
+})
+export class BasicBottomSheetModule {}

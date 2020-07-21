edbe84aaad03e79085680a8f91fefe50955097ea
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-07-21 08:10:25 -0400

    Try to fix codesandbox

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Needed for codesandbox to work
+import 'core-js/es/reflect';
+import 'core-js/stable/reflect';
+import 'core-js/features/reflect';
+
+
 if (environment.production) {
     enableProdMode();
 }

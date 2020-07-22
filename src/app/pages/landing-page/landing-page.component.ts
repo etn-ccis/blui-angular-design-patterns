8fb1e3843d05414433a787df47b696dc48c8faf2
daileytj <daileytj89@gmail.com>
2020-07-22 12:17:51 -0400

    Add animations, hide app-bar on large screens

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -2,11 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { StateService } from 'src/app/services/state.service';
 import * as Colors from '@pxblue/colors';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing-page.component.html',
     styleUrls: ['./landing-page.component.scss'],
+    animations: [
+        trigger('headerFadeInAnimation', [
+            state('in', style({ opacity: 1 })),
+
+            transition(':enter', [style({ opacity: 0 }), animate('500ms 250ms')]),
+        ]),
+        trigger('bodyFadeInAnimation', [
+            state('in', style({ opacity: 1 })),
+
+            transition(':enter', [style({ opacity: 0 }), animate('1500ms 500ms')]),
+        ]),
+        trigger('linksFadeInAnimation', [
+            state('in', style({ opacity: 1 })),
+
+            transition(':enter', [style({ opacity: 0 }), animate('1000ms 1250ms')]),
+        ]),
+    ],
 })
 export class LandingPageComponent implements OnInit {
     Colors = Colors;
@@ -20,8 +38,8 @@ export class LandingPageComponent implements OnInit {
     ngOnInit(): void {
         this._breakpointObserver
             .observe([Breakpoints.Small, Breakpoints.Handset])
-            .subscribe((state: BreakpointState) => {
-                if (state.matches) {
+            .subscribe((breakpointState: BreakpointState) => {
+                if (breakpointState.matches) {
                     this.isSmall = true;
                 } else {
                     this.isSmall = false;

27030d6004516cbc6f896020da87769565e802ac
daileytj <daileytj89@gmail.com>
2020-07-22 12:21:35 -0400

    Add browseranimatino module to fix unit tests

--- src/app/pages/landing-page/landing-page.module.ts
+++ src/app/pages/landing-page/landing-page.module.ts
@@ -5,9 +5,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LandingPageComponent } from './landing-page.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, FlexLayoutModule],
+    imports: [
+        CommonModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        FlexLayoutModule,
+    ],
     declarations: [LandingPageComponent],
 })
 export class LandingPageModule {}

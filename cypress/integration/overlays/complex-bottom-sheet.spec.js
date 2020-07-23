2ecd7c3683df50ef133b3ca75f8121def95de5c4
Greiner <E0458886@eaton.ad.etn.com>
2020-07-23 14:48:59 -0400

    Add dismiss test for bottom sheet

--- cypress/integration/overlays/complex-bottom-sheet.spec.js
+++ cypress/integration/overlays/complex-bottom-sheet.spec.js
@@ -22,4 +22,11 @@ describe('Complex bottom sheet', () => {
         .and('contain', 'Session')
         cy.get('[data-cy=btm-sheet-close]').should('contain', 'Close')
     });
+
+    it('should dismiss when page is selected', () => {
+        cy.get('[data-cy=action-menu]').click()
+        cy.get('[data-cy=btm-sheet-sort]').should('be.visible')
+        cy.get('.cdk-overlay-backdrop').click({ force : true})
+        cy.get('[data-cy=btm-sheet-sort]').should('not.be.visible')
+    });
 });

96a88d4ebd7d6d4faeb3d860412db3b2d834a0fe
huayunh <hhuayun0828@gmail.com>
2020-08-22 22:29:10 -0400

    Use sticky cancel button

--- src/app/pages/overlays/complex-bottom-sheet/bottom-sheet/bottom-sheet.ts
+++ src/app/pages/overlays/complex-bottom-sheet/bottom-sheet/bottom-sheet.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation, NgModule, OnInit } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
 import { InfoListItemModule } from '@pxblue/angular-components';
 import { DataService } from '../data.service';

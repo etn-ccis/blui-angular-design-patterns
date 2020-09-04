c5ecef8576cb84e38ea7417e48b16d0e77723855
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-09-04 12:27:06 -0400

    Run prettier

--- src/app/pages/form-validation/password-strength-check.component.ts
+++ src/app/pages/form-validation/password-strength-check.component.ts
@@ -6,17 +6,18 @@ import * as Colors from '@pxblue/colors';
     template: `
         <mat-list-item style="height: auto;">
             <mat-icon [style.color]="success ? Colors.blue[500] : Colors.gray[300]">{{ icon }}</mat-icon>
-            <span [style.color]="success ? Colors.gray[100] : Colors.gray[300]" style="padding-left: 8px;">{{ validationMessage }}</span>
+            <span [style.color]="success ? Colors.gray[100] : Colors.gray[300]" style="padding-left: 8px;">{{
+                validationMessage
+            }}</span>
         </mat-list-item>
     `,
     styles: [
-      `
-        ::ng-deep .mat-list-base .mat-list-item .mat-list-item-content {
-          padding: 0;
-        }
-      `
-    ]
-
+        `
+            ::ng-deep .mat-list-base .mat-list-item .mat-list-item-content {
+                padding: 0;
+            }
+        `,
+    ],
 })
 export class PasswordStrengthCheckComponent {
     @Input() icon = 'done';

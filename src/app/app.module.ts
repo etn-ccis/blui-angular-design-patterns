1052514fe9a9be71eedb43c893a946670455bddc
Evan <evan.w.mclaughlin@gmail.com>
2021-07-22 10:31:57 -0400

    Add license

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,10 @@
+/**
+ Copyright (c) 2021-present, Eaton
+
+ All rights reserved.
+
+ This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+ **/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';

c364d213ac64e4a171b44fb9b1850e2764f96002
Ekta Ghag <ektasawant99@gmail.com>
2021-07-30 17:30:00 +0530

    fix failing test case

--- src/app/pages/forms-and-validation/fix-length-passcode/fix-length-passcode.component.spec.ts
+++ src/app/pages/forms-and-validation/fix-length-passcode/fix-length-passcode.component.spec.ts
@@ -1,10 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FixLengthPasscodeComponent } from './fix-length-passcode.component';
+import { FixLengthPasscodeModule } from './fix-length-passcode.module';
 
 describe('FixLengthPasscodeComponent', () => {
     let component: FixLengthPasscodeComponent;
     let fixture: ComponentFixture<FixLengthPasscodeComponent>;
+
+    beforeEach(async(() => {
+        void TestBed.configureTestingModule({
+            imports: [FixLengthPasscodeModule],
+        }).compileComponents();
+    }));
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({

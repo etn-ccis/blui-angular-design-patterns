af82bc4511e0aa768e1af568bc6b3eb118d38c89
Ekta Ghag <ektasawant99@gmail.com>
2021-08-20 19:21:23 +0530

    remove unwanted modules

--- src/app/pages/forms-and-validation/in-a-list/in-a-list.module.ts
+++ src/app/pages/forms-and-validation/in-a-list/in-a-list.module.ts
@@ -3,13 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
@@ -28,13 +22,7 @@ import { InAListComponent } from './in-a-list.component';
         CommonModule,
         MatToolbarModule,
         MatIconModule,
-        MatButtonModule,
-        MatInputModule,
         MatFormFieldModule,
-        MatDividerModule,
-        MatSelectModule,
-        MatTooltipModule,
-        MatCheckboxModule,
         MatCardModule,
         MatSlideToggleModule,
         InfoListItemModule,

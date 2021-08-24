6ee99786cb2e80b146342828b78dd628f9c89c7b
Ekta Ghag <ektasawant99@gmail.com>
2021-08-24 11:49:28 +0530

    input styling, remove mobile padding

--- src/app/pages/forms-and-validation/in-a-list/in-a-list.module.ts
+++ src/app/pages/forms-and-validation/in-a-list/in-a-list.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
@@ -22,6 +23,7 @@ import { InAListComponent } from './in-a-list.component';
         CommonModule,
         MatToolbarModule,
         MatIconModule,
+        MatInputModule,
         MatFormFieldModule,
         MatCardModule,
         MatSlideToggleModule,

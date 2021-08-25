10d30b6b5108a9d065e9b6eee0ca9ecaa196e0d8
Ekta Ghag <ektasawant99@gmail.com>
2021-08-25 22:31:59 +0530

    add table in form

--- src/app/pages/forms-and-validation/in-a-table/in-a-table.component.ts
+++ src/app/pages/forms-and-validation/in-a-table/in-a-table.component.ts
@@ -2,6 +2,8 @@ import {
   Component,
   OnInit,
 } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { StateService } from '../../../services/state.service';
@@ -21,40 +23,102 @@ type DataItem = {
 export class InATableComponent implements OnInit {
   isSmall: boolean;
   data: DataItem[] = [];
-  displayedColumns: string[] = ['id', 'name', 'min', 'max'];
+  displayedColumns1: string[] = ['id', 'name', 'min', 'max'];
+
+  form: FormGroup;
+
+
+  displayedColumns = ['id', 'name', 'min', 'max'];
+  dataSource: MatTableDataSource<AbstractControl>;
+
   constructor(
     private readonly _drawerService: StateService,
-        private readonly _breakpointObserver: BreakpointObserver,
-  ) { }
+    private readonly _breakpointObserver: BreakpointObserver,
+    private readonly _fb: FormBuilder,
+  ) {
+
+    this.createForm();
+    this.addRow();
+    this.dataSource = new MatTableDataSource(this.tableControlArray.controls);
+  }
+
+
+
+
+  get tableControlArray() {
+    return this.form.get('tableExample') as FormArray;
+  }
+
+  createForm() {
+    this.form = this._fb.group({
+      tableExample: this._fb.array([]),
+    });
+  }
+
+  // trackRows(index: number, row: AbstractControl) {
+  //   return row.value.uid;
+  // }
+
+  private addRow() {
+    const rows = this.tableControlArray;
+    const name = [
+      {
+        name: 'Power',
+        min: 123,
+        max: 456,
+      },
+      {
+        name: 'Xpert',
+        min: 123,
+        max: 456,
+      },
+      {
+        name: 'Blue',
+        min: 123,
+        max: 456,
+      },
+    ];
+
+    name.map((element) => {
+      rows.push(
+        this._fb.group({
+          name: [element.name, Validators.required],
+          min: [element.min, Validators.required],
+          max: [element.max, Validators.required],
+        })
+      );
+    });
+
+  }
 
   ngOnInit(): void {
     this._breakpointObserver
-            .observe([Breakpoints.Small, Breakpoints.Handset])
-            .subscribe((state: BreakpointState) => {
-                if (state.matches) {
-                    this.isSmall = true;
-                } else {
-                    this.isSmall = false;
-                }
-            });
+      .observe([Breakpoints.Small, Breakpoints.Handset])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.isSmall = true;
+        } else {
+          this.isSmall = false;
+        }
+      });
 
-        this.data = [
-          {
-            id: 1,
-            name: 'power',
-            min: 10,
-            max: 20,
-          },
-          {
-            id: 2,
-            name: 'power-1',
-            min: 40,
-            max: 20,
-          },
-        ]
+    this.data = [
+      {
+        id: 1,
+        name: 'power',
+        min: 10,
+        max: 20,
+      },
+      {
+        id: 2,
+        name: 'power-1',
+        min: 40,
+        max: 20,
+      },
+    ]
   }
   toggleMenu(): void {
     const drawerOpen = this._drawerService.getDrawerOpen();
     this._drawerService.setDrawerOpen(!drawerOpen);
-}
+  }
 }

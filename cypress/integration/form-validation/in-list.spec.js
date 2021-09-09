99a87333998e9aefa965862dde1a4673d068df56
jeffvg <jeffvgreiner@eaton.com>
2021-09-09 16:12:45 -0400

    add more cypress tests

---
+++ cypress/integration/form-validation/in-list.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+
+describe('Form validation', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/forms-and-validation/in-a-list');
+    });
+
+    it('should display page title', () => {
+        cy.get('[data-cy=pxb-toolbar]').should('contain', 'In a List');
+    });
+
+    it('should display ip address left aligned', () => {
+        cy.get('[data-cy=ip-address]').invoke('css', 'text-align').should('equal', 'left')
+    });
+
+    it('should display disabled toggle in #424e54 opacity 0.3', () => {
+        cy.get('.mat-slide-toggle-bar').invoke('css', 'background-color').should('equal', 'rgba(66, 78, 84, 0.3)')
+    });
+
+    it('should display enabled toggle in #0088f2 opacity 0.5 ', () => {
+        cy.get('.mat-slide-toggle-bar').click()
+        .invoke('css', 'background-color').should('equal', 'rgba(0, 136, 242, 0.5)')
+    });
+});
--- cypress/integration/form-validation/table.spec.js
+++ cypress/integration/form-validation/table.spec.js
@@ -9,4 +9,16 @@ describe('Form validation', () => {
     it('should display page title', () => {
         cy.get('[data-cy=pxb-toolbar]').should('contain', 'In A Table');
     });
+
+    it('should have Min placeholder', () => {
+        cy.get('#mat-input-0').invoke('attr', 'placeholder').should('contain', 'Min')
+    });
+
+    it('should have Max placeholder', () => {
+        cy.get('#mat-input-1').invoke('attr', 'placeholder').should('contain', 'Max')
+    });
+
+    it('should display Min Max align right', () => {
+        cy.get('#mat-input-1').invoke('css', 'text-align').should('equal', 'right')
+    });
 });

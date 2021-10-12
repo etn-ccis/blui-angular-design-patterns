e67a0a6d801b3d5bd2f950ba20093f4b8b54c10c
Ekta Ghag <ektasawant99@gmail.com>
2021-10-12 12:17:38 +0530

    fix app bar right content

--- src/app/pages/list/status-list/status-list.module.ts
+++ src/app/pages/list/status-list/status-list.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { StatusListComponent } from './status-list.component';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,12 +14,13 @@ import { InfoListItemModule, ListItemTagModule } from '@pxblue/angular-component
         BrowserAnimationsModule,
         CommonModule,
         InfoListItemModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatListModule,
-        MatButtonModule,
         ListItemTagModule,
+        MatBadgeModule,
+        MatButtonModule,
         MatExpansionModule,
+        MatListModule,
+        MatIconModule,
+        MatToolbarModule,
     ],
     declarations: [StatusListComponent],
 })

4cf44996162ac9315f12d643a3946f9f4e63b2ae
Evan <evan.w.mclaughlin@gmail.com>
2021-11-03 15:22:45 -0400

    Run prettier

--- src/app/pages/list/tree/tree.module.ts
+++ src/app/pages/list/tree/tree.module.ts
@@ -9,7 +9,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatRadioModule } from '@angular/material/radio';
 import { TreeItemComponent } from './tree-item.component';
 import { FormsModule } from '@angular/forms';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [

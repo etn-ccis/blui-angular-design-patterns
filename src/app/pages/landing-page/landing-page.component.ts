8b03f7a6c754d56a105e7c91ff63344fc968b4ba
Andrupu <47467951+andrupu@users.noreply.github.com>
2021-11-05 16:02:24 -0400

    Rebrand Landing Page component

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { StateService } from '../../services/state.service';
-import * as Colors from '@pxblue/colors';
+import * as Colors from '@brightlayer-ui/colors';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({

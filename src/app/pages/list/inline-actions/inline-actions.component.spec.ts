c1b3f9b2a3fc3d42b0b4a44de4504f4c9f9b25b2
Ekta Ghag <ektasawant99@gmail.com>
2021-12-23 13:03:30 +0530

    update mobile view

--- src/app/pages/list/inline-actions/inline-actions.component.spec.ts
+++ src/app/pages/list/inline-actions/inline-actions.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InlineActionsComponent } from './inline-actions.component';
 
 describe('InlineActionsComponent', () => {
-  let component: InlineActionsComponent;
-  let fixture: ComponentFixture<InlineActionsComponent>;
+    let component: InlineActionsComponent;
+    let fixture: ComponentFixture<InlineActionsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ InlineActionsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [InlineActionsComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InlineActionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InlineActionsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/pages/list/inline-actions/inline-actions.component.ts
+++ src/app/pages/list/inline-actions/inline-actions.component.ts
@@ -4,69 +4,71 @@ import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/l
 import * as Colors from '@brightlayer-ui/colors';
 
 export type Data = {
-  title: string;
-  showActionItems: boolean;
-  showTag: boolean;
+    title: string;
+    showActionItems: boolean;
+    showTag: boolean;
 };
 
 @Component({
-  selector: 'app-inline-actions',
-  templateUrl: './inline-actions.component.html',
-  styleUrls: ['./inline-actions.component.scss']
+    selector: 'app-inline-actions',
+    templateUrl: './inline-actions.component.html',
+    styleUrls: ['./inline-actions.component.scss'],
 })
 export class InlineActionsComponent implements OnInit {
-  colors = Colors;
-  data: Data[];
-  isSmall: boolean;
-  constructor(
-    private readonly _drawerService: StateService,
-    private readonly _breakpointObserver: BreakpointObserver
-  ) { }
+    colors = Colors;
+    data: Data[];
+    isSmall: boolean;
+    constructor(
+        private readonly _drawerService: StateService,
+        private readonly _breakpointObserver: BreakpointObserver
+    ) {}
 
-  ngOnInit(): void {
-    this.loadTable();
-    this._breakpointObserver
-      .observe([Breakpoints.Small, Breakpoints.Handset])
-      .subscribe((state: BreakpointState) => {
-        if (state.matches) {
-          this.isSmall = true;
-        } else {
-          this.isSmall = false;
-        }
-      });
-  }
+    ngOnInit(): void {
+        this.loadTable();
+        this._breakpointObserver
+            .observe([Breakpoints.Small, Breakpoints.Handset])
+            .subscribe((state: BreakpointState) => {
+                if (state.matches) {
+                    this.isSmall = true;
+                } else {
+                    this.isSmall = false;
+                }
+            });
+    }
 
-  toggleMenu(): void {
-    const drawerOpen = this._drawerService.getDrawerOpen();
-    this._drawerService.setDrawerOpen(!drawerOpen);
-  }
+    toggleMenu(): void {
+        const drawerOpen = this._drawerService.getDrawerOpen();
+        this._drawerService.setDrawerOpen(!drawerOpen);
+    }
 
-  showAction(index: number, showBtn: boolean): void {
-    this.data[index].showActionItems = showBtn;
-  }
+    showAction(index: number, showBtn: boolean): void {
+        this.data[index].showActionItems = showBtn;
+    }
 
-  hideAction(index: number, showBtn: boolean): void {
-    this.data[index].showActionItems = showBtn;
-  }
+    hideAction(index: number, showBtn: boolean): void {
+        this.data[index].showActionItems = showBtn;
+    }
 
-  deleteActions(index: number): void {
-    this.data.splice(index,1);
-  }
-  loadTable(): void {
-    this.data = [{
-      title: 'High Humidity',
-      showActionItems: false,
-      showTag: true,
-    },
-    {
-      title: 'Battery Service',
-      showActionItems: false,
-      showTag: false,
-    },
-    {
-      title: 'Bypass Over Frequency',
-      showActionItems: false,
-      showTag: false,
-    }];
-  }
+    deleteActions(index: number): void {
+        this.data.splice(index, 1);
+    }
+    loadTable(): void {
+        this.data = [
+            {
+                title: 'High Humidity',
+                showActionItems: false,
+                showTag: true,
+            },
+            {
+                title: 'Battery Service',
+                showActionItems: false,
+                showTag: false,
+            },
+            {
+                title: 'Bypass Over Frequency',
+                showActionItems: false,
+                showTag: false,
+            },
+        ];
+    }
 }
